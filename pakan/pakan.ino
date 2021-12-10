2e718f119bab220fd310dac1def3d71cc026be9f
bywahjoe <wahjoe.why@gmail.com>
2021-12-10 20:43:03 +0700

    V1.0

---
+++ pakan/pakan.ino
@@ -0,0 +1,15 @@
+/* Firmansyah Wahyu @2021
+ * Repo :https://github.com/bywahjoe/pakanUNP.git 
+ */
+
+#include "pinku.h"
+#include "datapakan.h"
+
+void setup() {
+  // put your setup code here, to run once:
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
